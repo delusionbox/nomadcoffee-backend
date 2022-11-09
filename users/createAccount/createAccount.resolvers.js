0f7121be27f8050a217a8f850677f06885c888fd
Docofarm <changhyeonseo283@gmail.com>
2022-11-09 13:49:10 +0900

    create user

---
+++ users/createAccount/createAccount.resolvers.js
@@ -0,0 +1,51 @@
+import bcrypt from "bcrypt";
+import client from "../../client";
+
+export default {
+    Mutation: {
+        createAccount: async (_, {
+            username, email, name, location, password, avatarURL, githubUsername
+        }) => {
+            try {
+                //username or email check
+                const existUser = await client.user.findFirst({
+                    where: {
+                        OR: [{
+                            username,
+                        },
+                        {
+                            email,
+                        },
+                        ],
+                    },
+                });
+                if (existUser) {
+                    throw new Error("Check that the username / email aren't taken.");
+                }
+                //password hashing
+                const uglyPassword = await bcrypt.hash(password, 10);
+                //user create
+                await client.user.create({
+                    data: {
+                        username,
+                        email,
+                        name,
+                        location,
+                        avatarURL,
+                        githubUsername,
+                        password: uglyPassword,
+                    },
+                });
+                //ok or error
+                return {
+                    ok: true,
+                }
+            } catch ($error) {
+                return {
+                    ok: false,
+                    error: $error
+                }
+            }
+        },
+    },
+};
